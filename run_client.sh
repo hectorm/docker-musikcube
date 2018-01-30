69ede220ca7940bb40c4858ca3f298c432d5e973
Héctor Molinero Fernández <hector@molinero.xyz>
2018-01-30 20:36:03 +0100

    Use musikcube daemon

--- run_client.sh
+++ run_client.sh
@@ -7,14 +7,20 @@ if [ -d "${HOME}/Music" ]; then
 	CONTAINER_MUSIC_FOLDER='/music'
 fi
 
+if [ -S "${XDG_RUNTIME_DIR:-}/pulse/native" ]; then
+	HOST_PULSEAUDIO_SOCKET="${XDG_RUNTIME_DIR}/pulse/native"
+	CONTAINER_PULSEAUDIO_SOCKET='/run/user/1000/pulse/native'
+fi
+
 docker rm --force musikcube 2>/dev/null || true
-docker run --tty --detach \
+docker run --tty --interactive --rm \
 	--name musikcube \
 	--cpus 1 \
 	--memory 128mb \
 	--publish 7905:7905 \
 	--publish 7906:7906 \
-	--restart on-failure:10 \
+	--env USE_MUSIKCUBE_CLIENT=1 \
+	--env MUSIKCUBE_OUTPUT_DRIVER='AlsaOut' \
 	--env MUSIKCUBE_SERVER_PASSWORD='musikcube' \
 	--env CLOUDFLARE_EMAIL='email@example.com' \
 	--env CLOUDFLARE_API_KEY='xxxxxxxxxxxxxxx' \
@@ -22,5 +28,9 @@ docker run --tty --detach \
 	--mount type=volume,src='musikcube-app-data',dst='/home/musikcube/.musikcube' \
 	${HOST_MUSIC_FOLDER:+ \
 		--mount type=bind,src="${HOST_MUSIC_FOLDER}",dst="${CONTAINER_MUSIC_FOLDER}",ro \
+	} \
+	${HOST_PULSEAUDIO_SOCKET:+ \
+		--mount type=bind,src="${HOST_PULSEAUDIO_SOCKET}",dst="${CONTAINER_PULSEAUDIO_SOCKET}",ro \
+		--env PULSE_SERVER="${CONTAINER_PULSEAUDIO_SOCKET}" \
 	} \
 	musikcube
